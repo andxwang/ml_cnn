958c8ac66b192fdb6d6138330bbbc6ecf8ace160
andxwang <therainruffy@yahoo.com>
2024-02-06 00:26:19 +0000

    create dataset and dataloader

---
+++ data_utils.py
@@ -0,0 +1,45 @@
+# data_utils.py
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+
+# Custom dataset class for CIFAR-10
+class CIFAR10Dataset(Dataset):
+    def __init__(self, data, targets, transform=None):
+        self.data = data
+        self.targets = targets
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        x = self.data[index]
+        y = self.targets[index]
+
+        if self.transform:
+            x = self.transform(x)
+
+        return x, y
+
+# Function to create a DataLoader for CIFAR-10
+def create_cifar10_dataloader(batch_size, shuffle=True):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=shuffle)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = DataLoader(testset, batch_size=batch_size, shuffle=shuffle)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')  # TODO: change to use class id method
+
+    return trainloader, testloader, classes
